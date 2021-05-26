398a076f8a9d7ad2cf3ac6b1407570a2fa8ef891
Efe <efeurun@gmail.com>
2021-05-26 21:31:39 +0300

    added RectTransformsExtensions.cs which can be used for overlapping UI objects.

---
+++ RectTransformExtensions.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public static class RectTransformExtensions
+{
+
+    public static bool Overlaps(this RectTransform a, RectTransform b)
+    {
+        return a.WorldRect().Overlaps(b.WorldRect());
+    }
+    public static bool Overlaps(this RectTransform a, RectTransform b, bool allowInverse)
+    {
+        return a.WorldRect().Overlaps(b.WorldRect(), allowInverse);
+    }
+
+    public static Rect WorldRect(this RectTransform rectTransform)
+    {
+        Vector2 sizeDelta = rectTransform.sizeDelta;
+        float rectTransformWidth = sizeDelta.x * rectTransform.lossyScale.x;
+        float rectTransformHeight = sizeDelta.y * rectTransform.lossyScale.y;
+
+        Vector3 position = rectTransform.position;
+        return new Rect(position.x - rectTransformWidth / 2f, position.y - rectTransformHeight / 2f, rectTransformWidth, rectTransformHeight);
+    }
+}
+
+//OR
+//bool rectOverlaps(RectTransform rectTrans1, RectTransform rectTrans2)
+//{
+//    Rect rect1 = new Rect(rectTrans1.localPosition.x, rectTrans1.localPosition.y, rectTrans1.rect.width, rectTrans1.rect.height);
+//    Rect rect2 = new Rect(rectTrans2.localPosition.x, rectTrans2.localPosition.y, rectTrans2.rect.width, rectTrans2.rect.height);
+
+//    return rect1.Overlaps(rect2);
+//}
+
+//public RectTransform uiRect1;
+//public RectTransform uiRect2;
+
+//void Update()
+//{
+//    if (rectOverlaps(uiRect1, uiRect2))
+//    {
+//        Debug.Log("Overlaps");
+//    }
+//    else
+//    {
+//        Debug.Log("Does not Overlap");
+//    }
+//}
